5e732d2fcc95ec846b54faf376c6d5da789cf5b0
jinzhao <467847281@qq.com>
2020-08-12 13:18:05 +0800

    sss

---
+++ .doom.d/custom.el
@@ -0,0 +1,99 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282c34" "#ff8080" "#95ffa4" "#ffe9aa" "#91ddff" "#c991e1" "#aaffe4" "#cbe3e7"])
+ '(ccls-args '("--log-file=/tmp/1.log"))
+ '(ccls-initialization-options
+   '(:cache
+     (:directory "/tmp/ccls-cache/")
+     :compilationDatabaseDirectory "build/" :completion
+     (:filterAndSort t)))
+ '(company-idle-delay 0 t)
+ '(company-transformers nil t)
+ '(compilation-error-regexp-alist
+   '(absoft ada aix ant bash borland python-tracebacks-and-caml cmake cmake-info comma cucumber msft edg-1 edg-2 epc ftnchek iar ibm irix java jikes-file maven jikes-line clang-include gcc-include ruby-Test::Unit gnu lcc makepp mips-1 mips-2 msft omake oracle perl php rxp sparc-pascal-file sparc-pascal-line sparc-pascal-example sun sun-ada watcom 4bsd gcov-file gcov-header gcov-nomark gcov-called-line gcov-never-called perl--Pod::Checker perl--Test perl--Test2 perl--Test::Harness weblint guile-file guile-line msbuild))
+ '(custom-enabled-themes '(doom-molokai))
+ '(custom-safe-themes
+   '("be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "c6b93ff250f8546c7ad0838534d46e616a374d5cb86663a9ad0807fd0aeb1d16" "4a9f595fbffd36fe51d5dd3475860ae8c17447272cf35eb31a00f9595c706050" "a4b9eeeabde73db909e6b080baf29d629507b44276e17c0c411ed5431faf87dd" "a4fa3280ffa1f2083c5d4dab44a7207f3f7bcb76e720d304bd3bd640f37b4bef" "a02836a5807a687c982d47728e54ff42a91bc9e6621f7fe7205b0225db677f07" "de43de35da390617a5b3e39b6b27c107cc51271eb95cceb1f43d13d9647c911d" "92d8a13d08e16c4d2c027990f4d69f0ce0833c844dcaad3c8226ae278181d5f3" "a9c619535d63719a15f22e3c450a03062d3fed1e356ef96d33015849c4c43946" "dc677c8ebead5c0d6a7ac8a5b109ad57f42e0fe406e4626510e638d36bcc42df" "428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "7d56fb712ad356e2dacb43af7ec255c761a590e1182fe0537e1ec824b7897357" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "7ffb0d3d0c797b980ed7330adc04a66516d49a61e4187a7054dda014676421d9" "2d392972cbe692ee4ac61dc79907af65051450caf690a8c4d36eb40c1857ba7d" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "2a3ffb7775b2fe3643b179f2046493891b0d1153e57ec74bbe69580b951699ca" "c8f959fb1ea32ddfc0f50db85fea2e7d86b72bb4d106803018be1c3566fd6c72" "f8deca10cbe6cd68cc71b891c661a807ef25b49ea5c832af192b9663f3f8c8a7" "84e7c46be3e80c8e1eaf72884855cc98bb6957219be82f217c0100f42973340f" "e838d6375a73fda607820c65eb3ea1f9336be7bd9a5528c9161e10c4aa663b5b" "4201f4d3ce08da82c08aca12d9ca2c085bb07c115c096b36f111a34f0cf8d11d" "155a5de9192c2f6d53efcc9c554892a0d87d87f99ad8cc14b330f4f4be204445" "fa540ee33c78f23a88b56393fa4865e61f7f0244f19f80123094a7d2d0a9c0ec" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" "a8c210aa94c4eae642a34aaf1c5c0552855dfca2153fa6dd23f3031ce19453d4" "43c0beee9e16258acaa03a81c20d11a5678af81099ba7df708224d65abc5ec2e" "49ec957b508c7d64708b40b0273697a84d3fee4f15dd9fc4a9588016adee3dad" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" default))
+ '(default-file-name-coding-system 'utf-8 t)
+ '(dtrt-indent-run-after-smie t)
+ '(evil-surround-pairs-alist
+   '((40 "(" . ")")
+     (91 "[" . "]")
+     (123 "{" . "}")
+     (41 "(" . ")")
+     (93 "[" . "]")
+     (125 "{" . "}")
+     (35 "#{" . "}")
+     (98 "(" . ")")
+     (66 "{" . "}")
+     (62 "<" . ">")
+     (116 . evil-surround-read-tag)
+     (60 . evil-surround-read-tag)
+     (102 . evil-surround-function)))
+ '(fci-rule-color "#858FA5")
+ '(global-linum-mode nil)
+ '(hl-todo-highlight-punctuation ":")
+ '(idle-update-delay 1)
+ '(indent-tabs-mode nil)
+ '(jdee-db-active-breakpoint-face-colors (cons "#100e23" "#906cff"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#100e23" "#95ffa4"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#100e23" "#565575"))
+ '(lsp-clients-clangd-args '("-background-index" "--compile-commands-dir=build"))
+ '(lsp-clients-clangd-executable "clangd.exe")
+ '(lsp-enable-indentation nil)
+ '(lsp-enable-on-type-formatting nil)
+ '(lsp-ui-imenu-enable nil)
+ '(lsp-ui-imenu-kind-position 'left)
+ '(lsp-ui-peek-always-show nil)
+ '(lsp-ui-peek-enable nil)
+ '(lsp-ui-sideline-enable nil)
+ '(objed-cursor-color "#ff8080")
+ '(org-adapt-indentation nil)
+ '(org-highlight-sparse-tree-matches nil)
+ '(org-list-description-max-indent 4)
+ '(org-src-preserve-indentation t)
+ '(org-startup-indented t)
+ '(package-selected-packages '(evil-traces))
+ '(projectile-project-root-files-bottom-up '(".ccls-root" ".project"))
+ '(sp-highlight-pair-overlay nil)
+ '(sp-highlight-wrap-overlay nil)
+ '(sp-highlight-wrap-tag-overlay nil)
+ '(vc-annotate-background "#1b182c")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#95ffa4")
+    (cons 40 "#b8f7a6")
+    (cons 60 "#dbf0a8")
+    (cons 80 "#ffe9aa")
+    (cons 100 "#ffd799")
+    (cons 120 "#ffc488")
+    (cons 140 "#ffb378")
+    (cons 160 "#eda79b")
+    (cons 180 "#db9cbd")
+    (cons 200 "#c991e1")
+    (cons 220 "#db8bc0")
+    (cons 240 "#ed85a0")
+    (cons 260 "#ff8080")
+    (cons 280 "#d4757d")
+    (cons 300 "#aa6a7a")
+    (cons 320 "#805f77")
+    (cons 340 "#858FA5")
+    (cons 360 "#858FA5")))
+ '(vc-annotate-very-old-color nil)
+ '(yas-also-auto-indent-first-line t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-ui-peek-header ((t (:background "#e7ebf1" :foreground "light slate blue" :weight bold))))
+ '(lsp-ui-peek-highlight ((nil (:inherit lsp-ui-peek-header :background "#B8C5DB" :foreground "#E5E9F0" :box 1))))
+ '(lsp-ui-peek-list ((nil (:background "#ced1d8"))))
+ '(lsp-ui-peek-peek ((nil (:background "#ced1d8"))))
+ '(lsp-ui-peek-selection ((nil (:background "#3B6EA8" :distant-foreground "black" :box (:line-width 2 :color "grey75" :style released-button) :underline t :weight bold))))
+ '(nav-flash-face ((nil nil))))
+(put 'customize-group 'disabled nil)
