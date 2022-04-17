5b020c69047c5e26ff58f9ab02c9a9ed59bb5a87
paopaol <467847281@qq.com>
2022-04-17 21:31:31 +0800

    update

--- nvim/lua/completion/nvim-cmp.lua
+++ nvim/lua/completion/nvim-cmp.lua
@@ -2,8 +2,8 @@ local cmp = require 'cmp'
 local lspkind = require('lspkind')
 
 cmp.setup({
-  completion = {keyword_length = 1},
-  experimental = {view = {entries = "native"}},
+  completion = { keyword_length = 1 },
+  experimental = { view = { entries = "native" } },
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args) require('luasnip').lsp_expand(args.body) end
@@ -21,39 +21,43 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping(function()
       cmp.close()
       vim.cmd("startinsert!")
-    end, {'i'}),
+    end, { 'i' }),
     ['<C-f>'] = cmp.mapping(function()
       cmp.close()
       vim.cmd("normal! l")
       vim.cmd("startinsert")
-    end, {'s'}),
+    end, { 's' }),
     ['<C-b>'] = cmp.mapping(function()
       cmp.close()
       vim.cmd("normal! h")
       vim.cmd("startinsert")
-    end, {'s'}),
+    end, { 's' }),
     ['<C-a>'] = cmp.mapping(function()
       cmp.close()
       vim.cmd("normal! 0")
-    end, {'s'}),
+    end, { 's' }),
 
-    ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 'c'}),
-    ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 'c'}),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+    ['<backspace>'] = cmp.mapping(function()
+    end, { 's' }),
+    ['<delete>'] = cmp.mapping(function()
+    end, { 's' }),
+    ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<CR>'] = cmp.mapping.confirm({select = true}) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
   sources = cmp.config.sources({
-    {name = 'nvim_lsp'}, {name = 'buffer'}, {name = 'luasnip'}
+    { name = 'nvim_lsp' }, { name = 'buffer' }, { name = 'luasnip' }
   })
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
+cmp.setup.cmdline('/', { sources = { { name = 'buffer' } } })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
+  sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } })
 })
 
 --------------------------------------------------------------------------------
