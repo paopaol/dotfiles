c7be34b8b1fe9081bf039129bfee1346e1d8ddf5
paopaol <467847281.com>
2022-08-13 09:12:26 +0800

    update plugins

--- nvim/lua/lsp/accelerated.lua
+++ nvim/lua/lsp/accelerated.lua
@@ -1,4 +1,2 @@
-vim.cmd([[
-  nmap j <Plug>(accelerated_jk_gj)
-  nmap k <Plug>(accelerated_jk_gk)
-]])
+vim.api.nvim_set_keymap("n", "j", "<Plug>(accelerated_jk_gj)", {})
+vim.api.nvim_set_keymap("n", "k", "<Plug>(accelerated_jk_gk)", {})
