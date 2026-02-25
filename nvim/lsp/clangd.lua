b8aa9b5b3145c4c76d8a181c0a5a4e8dec9994c2
jinzhao <jinzhao>
2026-02-25 22:02:18 +0800

    perf():

--- nvim/lsp/clangd.lua
+++ nvim/lsp/clangd.lua
@@ -14,7 +14,13 @@ vim.lsp.config("clangd", {
     "--limit-results=20",
   },
   root_markers = { "compile_commands.json", "compile_flags.txt", ".git", ".projectile" },
-  on_attach = function(_, bufnr)
+  on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    if vim.api.nvim_buf_line_count(bufnr) > 5000 then
+      vim.lsp.semantic_tokens.enable(false, { bufnr = bufnr })
+      vim.diagnostic.enable(false, { bufnr = bufnr })
+      vim.notify("文件超过 5000 行，已禁用 LSP 语义高亮以提升性能", vim.log.levels.WARN)
+    end
   end,
 })
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,136 +1,137 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  require("colors.install"),
-  require("completion.install"),
-  require("menu.install"),
-  require("lang.install"),
-  require("edit.install"),
-  require("debugger.install"),
-  require("tools.install"),
+	require("colors.install"),
+	require("completion.install"),
+	require("menu.install"),
+	require("lang.install"),
+	require("edit.install"),
+	require("debugger.install"),
+	require("tools.install"),
 
-  { "ton/vim-bufsurf", event = "VeryLazy" },
+	{ "ton/vim-bufsurf",           event = "VeryLazy" },
 
-  { "nvim-neotest/nvim-nio", event = "VeryLazy" },
+	{ "nvim-neotest/nvim-nio",     event = "VeryLazy" },
 
-  { "akinsho/toggleterm.nvim", event = "VeryLazy" },
+	{ "akinsho/toggleterm.nvim",   event = "VeryLazy" },
 
-  { "nvim-lualine/lualine.nvim", event = "VeryLazy" },
+	{ "nvim-lualine/lualine.nvim", event = "VeryLazy" },
 
-  { "nvim-lua/popup.nvim", event = "VeryLazy" },
+	{ "nvim-lua/popup.nvim",       event = "VeryLazy" },
 
-  { "nvim-lua/plenary.nvim", event = "VeryLazy" },
+	{ "nvim-lua/plenary.nvim",     event = "VeryLazy" },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = "VeryLazy",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "go", "bash", "json" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "VeryLazy",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "go", "bash", "json" },
+				highlight = {
+					enable = true,
+					disable = { "c", "cpp" },
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
 
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    after = "nvim-treesitter",
-    requires = "nvim-treesitter/nvim-treesitter",
-  },
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		after = "nvim-treesitter",
+		requires = "nvim-treesitter/nvim-treesitter",
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    after = "nvim-treesitter",
-    requires = "nvim-treesitter/nvim-treesitter",
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		after = "nvim-treesitter",
+		requires = "nvim-treesitter/nvim-treesitter",
 
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        textobjects = {
-          select = {
-            enable = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-            },
-          },
-        },
-      })
-    end,
-  },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    -- event = "VeryLazy",
-    dependencies = { "saghen/blink.cmp" },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		-- event = "VeryLazy",
+		dependencies = { "saghen/blink.cmp" },
+	},
 
-  { "williamboman/mason.nvim", event = "VeryLazy" },
+	{ "williamboman/mason.nvim",         event = "VeryLazy" },
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      require("mason-lspconfig").setup({
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = require("blink.cmp").get_lsp_capabilities(),
-          })
-        end,
-      })
-    end,
-  },
+			require("mason-lspconfig").setup({
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = require("blink.cmp").get_lsp_capabilities(),
+					})
+				end,
+			})
+		end,
+	},
 
-  { "vim-scripts/bufkill.vim", event = "VeryLazy" },
+	{ "vim-scripts/bufkill.vim",         event = "VeryLazy" },
 
-  { "jose-elias-alvarez/null-ls.nvim", event = "VeryLazy" },
+	{ "jose-elias-alvarez/null-ls.nvim", event = "VeryLazy" },
 
-  { "onsails/lspkind-nvim", event = "VeryLazy" },
+	{ "onsails/lspkind-nvim",            event = "VeryLazy" },
 
-  { "paopaol/e-kaput.nvim", event = "VeryLazy" },
+	{ "paopaol/e-kaput.nvim",            event = "VeryLazy" },
 
-  { "ahmedkhalf/project.nvim", event = "VeryLazy" },
+	{ "ahmedkhalf/project.nvim",         event = "VeryLazy" },
 
-  { "kazhala/close-buffers.nvim", event = "VeryLazy" },
+	{ "kazhala/close-buffers.nvim",      event = "VeryLazy" },
 
-  { "tpope/vim-fugitive", event = "VeryLazy" },
+	{ "tpope/vim-fugitive",              event = "VeryLazy" },
 
-  { "sindrets/diffview.nvim", event = "VeryLazy", dependencies = "nvim-lua/plenary.nvim" },
+	{ "sindrets/diffview.nvim",          event = "VeryLazy", dependencies = "nvim-lua/plenary.nvim" },
 
-  {
-    "thinca/vim-qfreplace",
-    event = "VeryLazy",
-    config = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "qfreplace",
-        callback = function()
-          vim.g.auto_save = 0
-        end,
-      })
-    end,
-  },
+	{
+		"thinca/vim-qfreplace",
+		event = "VeryLazy",
+		config = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "qfreplace",
+				callback = function()
+					vim.g.auto_save = 0
+				end,
+			})
+		end,
+	},
 
-  { "akinsho/git-conflict.nvim", event = "VeryLazy", version = "*", config = true },
+	{ "akinsho/git-conflict.nvim", event = "VeryLazy", version = "*",  config = true },
 
-  { "j-hui/fidget.nvim", event = "VeryLazy", tag = "legacy", config = true },
+	{ "j-hui/fidget.nvim",         event = "VeryLazy", tag = "legacy", config = true },
 })
