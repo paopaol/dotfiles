41949ffe1f86d9bcc85147427cf527f7957b6f3b
jinzhao <jinzhao>
2026-03-31 13:16:46 +0800

    update

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -5,16 +5,10 @@ vim.opt_local.expandtab = true
 
 local utils = require("base.utils")
 
-local function command(cmd)
-  return function()
-    vim.cmd(cmd)
-  end
-end
-
 require("which-key").add({
   { "<localleader>,", utils.format_buffer, desc = "formatting", buffer = true },
-  { "<localleader>o", command("ClangdSwitchSourceHeader"), desc = "switch cc/h", buffer = true },
-  { "<localleader>g", command("TSCppDefineClassFunc"), desc = "define class func", buffer = true },
+  { "<localleader>o", utils.command("ClangdSwitchSourceHeader"), desc = "switch cc/h", buffer = true },
+  { "<localleader>g", utils.command("TSCppDefineClassFunc"), desc = "define class func", buffer = true },
 })
 
 require("clangd_extensions").setup({
--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -4,13 +4,7 @@ vim.bo.expandtab = true
 
 local utils = require("base.utils")
 
-local function command(cmd)
-  return function()
-    vim.cmd(cmd)
-  end
-end
-
 require("which-key").add({
   { "<localleader>,", utils.format_buffer, desc = "formatting", buffer = true },
-  { "<localleader>r", command("luafile %"), desc = "run current file", buffer = true },
+  { "<localleader>r", utils.command("luafile %"), desc = "run current file", buffer = true },
 })
